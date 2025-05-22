f3fdd90e98380f7eb1bdd68ecce00968558e2c58
Sachin kumar <kumarsachinkain2000@gmail.com>
2025-05-22 15:22:53 +0530

    modify to set dark mode by default

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -4,18 +4,25 @@ import { useEffect, useState } from "react"
 import { cn } from "@/lib/utils"
 
 const ThemeToggle = () =>{
-    const [isDarkMode , setIsDarkMode] = useState(true);
+    const [isDarkMode , setIsDarkMode] = useState(false);
 
-    useEffect(()=>{
-        const storedTheme = localStorage.getItem('theme');
-        if(storedTheme === 'dark'){
-            setIsDarkMode(true);
-            document.documentElement.classList.add('dark');
-        }else if(storedTheme === 'light'){
-            setIsDarkMode(false);
-            document.documentElement.classList.remove('dark');
-        }
-    },[])
+    useEffect(() => {
+  const storedTheme = localStorage.getItem('theme');
+
+  if (storedTheme === 'dark') {
+    setIsDarkMode(true);
+    document.documentElement.classList.add('dark');
+  } else if (storedTheme === 'light') {
+    setIsDarkMode(false);
+    document.documentElement.classList.remove('dark');
+  } else {
+    // DEFAULT to dark theme if nothing is stored
+    setIsDarkMode(true);
+    document.documentElement.classList.add('dark');
+    localStorage.setItem('theme', 'dark');
+  }
+}, []);
+
 
     const toggleTheme = ()=>{
       if(isDarkMode){
